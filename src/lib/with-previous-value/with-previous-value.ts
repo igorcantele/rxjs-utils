7c51f593284b9f2869e3f28da457176f7df930f4
Igor Cantele <76614313+igorcantele@users.noreply.github.com>
2024-03-16 19:19:38 +0100

    Feat/with previous values (#2)
    * chore/added JSDoc
    
    * test/added unit test for `withPreviousValue`
    
    * test/refactored tests for `loading.ts`

--- src/lib/with-previous-value/with-previous-value.ts
+++ src/lib/with-previous-value/with-previous-value.ts
@@ -1,6 +1,22 @@
-import { OperatorFunction, pipe } from "rxjs";
-import { map, pairwise, startWith } from "rxjs/operators";
+import {OperatorFunction, pipe} from "rxjs";
+import {map, pairwise, startWith} from "rxjs/operators";
 
+/**
+ * Operator that returns a stream containing the `current` and `previous` values.
+ *
+ * @returns stream containing`{current, previous}`.
+ *
+ * @example
+ * ```ts
+ * const stream$ = interval(500).pipe(
+ *   scan((acc, curr) => acc + 1, 0),
+ *   take(2),
+ *   withPreviousValue()
+ * );
+ * // { previous: undefined, current: 1 }
+ * // { previous: 1, current: 2 }
+ * ```
+ */
 export function withPreviousValue<T>(): OperatorFunction<
   T,
   {
