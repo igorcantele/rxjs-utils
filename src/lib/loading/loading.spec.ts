56ffccec4835655ea13e83ee1245a47a06c51c6b
Igor10798 <igor.cantele01@universitadipavia.it>
2024-03-16 18:28:18 +0100

    `beforeLoad` unit tests

--- src/lib/loading/loading.spec.ts
+++ src/lib/loading/loading.spec.ts
@@ -1,57 +1,110 @@
-import { of, Subject, throwError } from "rxjs";
-import { isLoading, prepare } from "./loading";
-import { switchMap } from "rxjs/operators";
+import {of, Subject, throwError} from "rxjs";
+import {beforeLoad, isLoading, prepare} from "./loading";
+import {bufferCount, switchMap} from "rxjs/operators";
 import createSpy = jasmine.createSpy;
 import createSpyObj = jasmine.createSpyObj;
 import SpyObj = jasmine.SpyObj;
 
 describe("prepare", () => {
-  it("should invoke callback upon subscription", done => {
-    const spy = createSpy("callback");
-    const observable = of("test").pipe(prepare(spy));
-    expect(spy).not.toHaveBeenCalled();
-    observable.subscribe(value => {
-      expect(spy).toHaveBeenCalled();
-      expect(value).toEqual("test");
-      done();
-    }, fail);
-  });
+    it("should invoke callback upon subscription", done => {
+        const spy = createSpy("callback");
+        const observable = of("test").pipe(prepare(spy));
+        expect(spy).not.toHaveBeenCalled();
+        observable.subscribe(value => {
+            expect(spy).toHaveBeenCalled();
+            expect(value).toEqual("test");
+            done();
+        }, fail);
+    });
 });
 
 describe("isLoading", () => {
-  let indicator: SpyObj<Subject<boolean>>;
-  let sink: Subject<string>;
+    let indicator: SpyObj<Subject<boolean>>;
+    let sink: Subject<string>;
 
-  beforeEach(() => {
-    indicator = createSpyObj("subject", ["next"]);
-    sink = new Subject<string>();
-  });
-  it("should switch indication based on subscription and completion", () => {
-    const observable = sink.pipe(isLoading(indicator));
-    expect(indicator.next).not.toHaveBeenCalled();
-    observable.subscribe(value => {
-      expect(value).toEqual("test");
-    }, fail);
-    expect(indicator.next.calls.mostRecent().args).toEqual([true]);
-    sink.next("test");
-    sink.complete();
-    expect(indicator.next.calls.mostRecent().args).toEqual([false]);
-  });
-
-  it("should switch indication based on observable erroring out", () => {
-    const observable = sink.pipe(
-      switchMap(() => throwError(new Error("error"))),
-      isLoading(indicator)
-    );
-    expect(indicator.next).not.toHaveBeenCalled();
-    observable.subscribe({
-      next: fail,
-      error: error => {
-        expect(error.message).toEqual("error");
-      }
+    beforeEach(() => {
+        indicator = createSpyObj("subject", ["next"]);
+        sink = new Subject<string>();
     });
-    expect(indicator.next.calls.mostRecent().args).toEqual([true]);
-    sink.next("test");
-    expect(indicator.next.calls.mostRecent().args).toEqual([false]);
-  });
+
+    it("should switch indication based on subscription and completion", () => {
+        const observable = sink.pipe(isLoading(indicator));
+        expect(indicator.next).not.toHaveBeenCalled();
+        observable.subscribe(value => {
+            expect(value).toEqual("test");
+        }, fail);
+        expect(indicator.next.calls.mostRecent().args).toEqual([true]);
+        sink.next("test");
+        sink.complete();
+        expect(indicator.next.calls.mostRecent().args).toEqual([false]);
+    });
+
+    it("should switch indication based on the observable erroring out", () => {
+        const observable = sink.pipe(
+            switchMap(() => throwError(new Error("EXPECTED_ERROR"))),
+            isLoading(indicator)
+        );
+        expect(indicator.next).not.toHaveBeenCalled();
+        observable.subscribe({
+            next: fail,
+            error: error => {
+                expect(error.message).toEqual("EXPECTED_ERROR");
+            }
+        });
+        expect(indicator.next.calls.mostRecent().args).toEqual([true]);
+        sink.next("test");
+        expect(indicator.next.calls.mostRecent().args).toEqual([false]);
+    });
+});
+
+describe("beforeLoad", () => {
+    it("should switch indication based on subscription", () => {
+        const eventEmitter = new Subject<number>
+        const targetObservable = eventEmitter.asObservable();
+        const beforeLoad$ = beforeLoad(targetObservable);
+
+        let expectedValue = true
+
+        beforeLoad$.subscribe({
+            next: value => {
+                expect(value).toEqual(expectedValue)
+            },
+            error: fail
+        })
+
+        expectedValue = false;
+        eventEmitter.next(1);
+        eventEmitter.next(1);
+    });
+
+    it("should switch indication based on subscription and the given filter function", () => {
+        const eventEmitter = new Subject<number>
+        const targetObservable = eventEmitter.asObservable();
+        const beforeLoad$ = beforeLoad(targetObservable, (num) => num !== 1);
+
+        let expectedValue = true
+
+        beforeLoad$.subscribe({
+            next: value => {
+                expect(value).toEqual(expectedValue)
+            },
+            error: fail
+        })
+
+        eventEmitter.next(1);
+        expectedValue = false;
+        eventEmitter.next(2);
+        eventEmitter.next(1);
+    });
+
+    it("should switch indication based on the observable erroring out", () => {
+        const beforeLoad$ = beforeLoad(throwError(new Error("EXPECTED_ERROR")));
+        beforeLoad$.pipe(bufferCount(2)).subscribe({
+            next: value => {
+                expect(value).toEqual([true, false])
+            },
+            error: fail
+        })
+
+    });
 });
