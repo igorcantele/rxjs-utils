9d0d36cf214c4db2b96bf6d1a3b30f64da88a19c
Igor Cantele <76614313+igorcantele@users.noreply.github.com>
2024-03-20 14:44:56 +0100

    Test/cascade strategies (#5)
    * chore/exported-error-on-cascade
    
    * test/added tests for multiple strategies in `CascadeStrategies`

--- src/lib/cascade-strategies/cascade-strategies.spec.ts
+++ src/lib/cascade-strategies/cascade-strategies.spec.ts
@@ -1,5 +1,5 @@
-import { CascadeStrategies } from "./cascade-strategies";
-import { of, Subscription } from "rxjs";
+import { ALL_STRATEGIES_FAILED, CascadeStrategies } from "./cascade-strategies";
+import { of, Subscription, throwError } from "rxjs";
 
 describe("CascadeStrategies", () => {
   const subscriptions: Subscription[] = [];
@@ -38,5 +38,101 @@ describe("CascadeStrategies", () => {
       expect(value).toEqual(null);
     }, fail);
     subscriptions.push(nullSubscription);
+  });
+
+  it("should succesfully use multiple strategies", () => {
+    const strategyMap = {
+      test1: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test2: (arg1: number, arg2: string) => of(arg2),
+      test3: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test4: (arg1: number, arg2: string) => of(arg2),
+      test5: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test6: (arg1: number, arg2: string) => of(arg2)
+    };
+    const cascadeStrategies = new CascadeStrategies(strategyMap);
+    Object.keys(strategyMap).forEach((key: keyof typeof strategyMap) => {
+      spyOn(strategyMap, key).and.callThrough();
+    });
+
+    const test1Observable = cascadeStrategies.use(["test1", "test2"], 0, "");
+    const test2Observable = cascadeStrategies.use(
+      ["test3", "test5", "test4"],
+      0,
+      ""
+    );
+    const test3Observable = cascadeStrategies.use(["test6"], 0, "");
+
+    const test1Subscription = test1Observable.subscribe(value => {
+      expect(strategyMap.test1).toHaveBeenCalled();
+      expect(strategyMap.test2).toHaveBeenCalled();
+      expect(value).toEqual("");
+    }, fail);
+    subscriptions.push(test1Subscription);
+    const test2Subscription = test2Observable.subscribe(value => {
+      expect(strategyMap.test3).toHaveBeenCalled();
+      expect(strategyMap.test4).toHaveBeenCalled();
+      expect(strategyMap.test6).toHaveBeenCalled();
+      expect(value).toEqual("");
+    }, fail);
+    subscriptions.push(test2Subscription);
+    const test3Subscription = test3Observable.subscribe(value => {
+      expect(value).toEqual("");
+    }, fail);
+    subscriptions.push(test3Subscription);
+  });
+
+  it("should fail if all strategies fails", () => {
+    const strategyMap = {
+      test1: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test2: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test3: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test4: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test5: (arg1: number, arg2: string) =>
+        throwError(new Error("TEST_ERROR")),
+      test6: (arg1: number, arg2: string) => throwError(new Error("TEST_ERROR"))
+    };
+    const cascadeStrategies = new CascadeStrategies(strategyMap);
+    Object.keys(strategyMap).forEach((key: keyof typeof strategyMap) => {
+      spyOn(strategyMap, key).and.callThrough();
+    });
+
+    const test1Observable = cascadeStrategies.use(["test1", "test2"], 0, "");
+    const test2Observable = cascadeStrategies.use(
+      ["test3", "test5", "test4"],
+      0,
+      ""
+    );
+    const test3Observable = cascadeStrategies.use(["test6"], 0, "");
+    const test4Observable = cascadeStrategies.use([], 0, "");
+
+    const test1Subscription = test1Observable.subscribe(fail, error => {
+      expect(strategyMap.test1).toHaveBeenCalled();
+      expect(strategyMap.test2).toHaveBeenCalled();
+      expect(error.message).toEqual(ALL_STRATEGIES_FAILED);
+    });
+    subscriptions.push(test1Subscription);
+    const test2Subscription = test2Observable.subscribe(fail, error => {
+      expect(strategyMap.test3).toHaveBeenCalled();
+      expect(strategyMap.test4).toHaveBeenCalled();
+      expect(strategyMap.test5).toHaveBeenCalled();
+      expect(error.message).toEqual(ALL_STRATEGIES_FAILED);
+    });
+    subscriptions.push(test2Subscription);
+    const test3Subscription = test3Observable.subscribe(fail, error => {
+      expect(strategyMap.test6).toHaveBeenCalled();
+      expect(error.message).toEqual(ALL_STRATEGIES_FAILED);
+    });
+    subscriptions.push(test3Subscription);
+    const test4Subscription = test4Observable.subscribe(fail, error => {
+      expect(error.message).toEqual(ALL_STRATEGIES_FAILED);
+    });
+    subscriptions.push(test4Subscription);
   });
 });
